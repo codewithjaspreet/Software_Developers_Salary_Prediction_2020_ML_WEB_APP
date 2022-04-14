78872eda7b5e583c6cef7fce6a727e2935003c66
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-14 15:12:10 +0530

    important files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,1 @@
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+web: sh setup.sh && streamlit run app.py
